79b99b464cdd7be6aa96cd18becea4fa90069c4b
yooook <654672373@qq.com>
2017-07-26 08:27:40 +0800

    'forth'

--- apskt.js
+++ apskt.js
@@ -11,6 +11,18 @@ var Apskt = function() {};
 
 // 框架基类方法
 Apskt.prototype = {
+    //去除左边空格
+    ltrim:function(str){
+        return str.replace(/(^\s*)/g,'');
+    },
+    //去除右边空格
+    rtrim:function(str){
+        return str.replace(/(\s*$)/g,'');
+    },
+    //去除空格
+    trim:function(str){
+        return str.replace(/(^\s*)|(\s*$)/g, '');
+    },
     // 实现对象拷贝，利于后面框架模块的划分
 	extend : function(target,source){
 		for(var i in source) {
@@ -142,7 +154,7 @@ Apskt.extend(Apskt,{
     //tag选择器
     getTag:function(tag,context){
         if(typeof context == 'string'){
-            context = Apskt.$id(context);
+            context = Apskt.getId(context);
         }
 
         if(context){
@@ -195,7 +207,7 @@ Apskt.extend(Apskt,{
                 pushArray(doms,result)
 
             }else if(first ==='#'){
-                doms=[Apskt.getId(item.slice(index+1))]//doms是数组，但是$id获取的不是数组，而是单个元素
+                doms=[Apskt.getId(item.slice(index+1))]//doms是数组，但是getId获取的不是数组，而是单个元素
                 pushArray(doms,result)
             }else{
                 doms = Apskt.getTag(item)
@@ -284,3 +296,196 @@ Apskt.extend(Apskt,{
         return  context.querySelectorAll(selector);
     },
 })
+
+
+// css框架
+Apskt.extend(Apskt,{
+    //样式
+    css:function(context, key, value){
+            var dom = Apskt.isString(context)?Apskt.query(context) : context;
+            //如果是数组
+            if(dom.length){
+                //如果value不为空，则表示设置
+                if(value){
+                    for(var i = dom.length - 1; i >= 0; i--){
+                        setStyle(dom[i],key, value);
+                    }
+                    //如果value为空，则表示获取
+                }else{
+                    return getStyle(dom[0]);
+                }
+                //如果不是数组
+            }else{
+                if(value){
+                    setStyle(dom,key, value);
+                }else{
+                    return getStyle(dom);
+                }
+            }
+            function getStyle(dom){
+                if(dom.currentStyle){
+                    return dom.currentStyle[key];
+                }else{
+                    return getComputedStyle(dom,null)[key];
+                }
+            }
+            function setStyle(dom,key,value){
+                dom.style[key] = value;
+            }
+    },
+
+    //显示
+    show:function (content){
+        var dom = Apskt.isString(context)?Apskt.query(context) : context;
+        for(var i= 0,len=doms.length;i<len;i++){
+            Apskt.css(doms[i], 'display', 'block');
+        }
+    },
+
+    //隐藏
+    hide:function (content){
+        var dom = Apskt.isString(context)?Apskt.query(context) : context;
+        for(var i= 0,len=doms.length;i<len;i++){
+            Apskt.css(doms[i], 'display', 'none');
+        }
+    },
+
+    //元素高度宽度概述
+    //计算方式：clientHeight clientWidth innerWidth innerHeight
+    //元素的实际高度+border，也不包含滚动条
+    Width:function (id){
+        return Apskt.getId(id).clientWidth
+    },
+
+    Height:function (id){
+        return Apskt.getId(id).clientHeight
+    },
+
+     //元素的滚动高度和宽度
+    //当元素出现滚动条时候，这里的高度有两种：可视区域的高度 实际高度（可视高度+不可见的高度）
+    //计算方式 scrollwidth
+    scrollWidth:function (id){
+        return Apskt.getId(id).scrollWidth
+    },
+
+    scrollHeight:function (id){
+        return Apskt.getId(id).scrollHeight
+    },
+
+    //元素滚动的时候 如果出现滚动条 相对于左上角的偏移量
+    //计算方式 scrollTop scrollLeft
+    scrollTop:function (id){
+        return Apskt.getId(id).scrollTop
+    },
+
+    scrollLeft:function (id){
+        return Apskt.getId(id).scrollLeft
+    },
+
+    //文档视口的高度和宽度
+    wWidth:function (){
+        return document.documentElement.clientWidth
+    },
+
+    wHeight:function (){
+        return document.documentElement.clientHeight
+    },
+
+    //文档滚动区域的整体的高和宽
+    wScrollHeight:function () {
+        return document.body.scrollHeight
+    },
+
+    wScrollWidth:function () {
+        return document.body.scrollWidth
+    },
+
+    //获取滚动条相对于其顶部的偏移
+    wScrollTop:function () {
+        var scrollTop = window.pageYOffset|| document.documentElement.scrollTop || document.body.scrollTop;
+        return scrollTop
+    },
+
+    //获取滚动条相对于其左边的偏移
+    wScrollLeft:function () {
+        var scrollLeft = document.body.scrollLeft || (document.documentElement && document.documentElement.scrollLeft);
+        return scrollLeft
+    }
+})
+
+
+// 属性框架
+Apskt.extend(Apskt,{
+    attr:function(content, key, value){
+        var dom = Apskt.isString(context)?Apskt.query(context) : context;
+        //如果是数组  比如tag
+            if(dom.length){
+                if(value){
+                    for(var i= 0, len=dom.length; i <len; i++){
+                        dom[i].setAttribute(key, value);
+                    }
+                }else{
+                    return dom[0].getAttribute(key);
+                }
+        //如果是单个元素  比如id
+            }else{
+                if(value){
+                    dom.setAttribute(key, value);
+                }else{
+                    return dom.getAttribute(key);
+                }
+            }
+    },
+
+    //动态添加和移除class
+    addClass:function (context, name){
+        var dom = Apskt.isString(context)?Apskt.query(context) : context;
+        //如果获取的是集合
+        if(doms.length){
+            for(var i= 0,len=doms.length;i<len;i++){
+                addName(doms[i]);
+            }
+        }else{
+            addName(doms);
+        }
+        function addName(dom){
+            dom.className = dom.className + ' ' + name;
+        }
+    },
+
+    removeClass:function (context, name){
+     var dom = Apskt.isString(context)?Apskt.query(context) : context;
+        if(doms.length){
+            for(var i= 0,len=doms.length;i<len;i++){
+                removeName(doms[i]);
+            }
+        }else{
+            removeName(doms);
+        }
+        function removeName(dom){
+            dom.className = dom.className.replace(name, '');
+        }
+    },
+
+    //判断是否有
+    hasClass:function(context,name){
+        var dom = Apskt.isString(context)?Apskt.query(context) : context;
+        var flag = true;
+        for(var i= 0,len=doms.length;i<len;i++){
+            flag = flag && check(doms[i],name)
+        }
+
+        return flag;
+        //判定单个元素
+        function check(element,name){
+            return -1<(" "+element.className+" ").indexOf(" "+name+" ")
+        }
+    },
+    
+    //获取
+    getClass:function (id){
+        var doms = $$.$all(id)
+        return Apskt.trim(doms[0].className).split(" ")
+    }
+})
+
